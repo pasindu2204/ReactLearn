25f3092b949e40696dc0687a1aeef867b72ac18c
Pasindu Amarasingha <chamipasindu2204@gmail.com>
2025-03-10 16:29:57 +0530

    add to new

---
+++ variableScope.js
@@ -0,0 +1,22 @@
+var _var = 10;
+let _let = 20;
+const _const = 30;
+
+//console.log(_var, _let, _const);
+
+function test() {
+    var _var2 = 100;
+    let _let2 = 200;
+    const _const2 =300;
+
+    if(true) {
+        var n1 = 22;
+        let n2 = 33;
+        const n3 = 44;
+    }
+ 
+    console.log(n1);
+    
+}
+
+test();
