68121c951268a9a81868dfec1b54399c6b65f98b
Pasindu Amarasingha <chamipasindu2204@gmail.com>
2025-03-10 16:35:05 +0530

    Create ArrowFuntions.js

---
+++ ArrowFuntions.js
@@ -0,0 +1,12 @@
+function greet (name){
+    return 'Hellow' + name + 'Student';
+
+}
+console.log(greet('Pasindu'));
+
+const arrow_greet = (name) => {
+
+    return 'Hellow' + name + 'Teacher in the School';
+
+}
+console.log(arrow_greet('Chamikara'));
