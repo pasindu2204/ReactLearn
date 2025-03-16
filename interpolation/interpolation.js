049f15a98455e146e684662d8456e020da5d9bed
Pasindu Amarasingha <chamipasindu2204@gmail.com>
2025-03-16 20:31:59 +0530

    interpolation

---
+++ interpolation/interpolation.js
@@ -0,0 +1,8 @@
+const name = "Pasindu";
+const age = 25;
+
+console.log('My Name is ' + name + '. I am ' + age + ' years old.');
+
+// using backtick key
+
+console.log(`My name is ${name}. I am ${age} years old.`);
---
+++ interpolation/templete.js
@@ -0,0 +1,11 @@
+const text = "This is a text.";
+
+console.log(text);
+
+//use backtick key
+
+const text2 = `This is 
+another 
+text.`;
+
+console.log(text2);
