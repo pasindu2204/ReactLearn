d7871abbc1e04422f8e85ca9eb8a16d28b9c2e12
Pasindu Amarasingha <chamipasindu2204@gmail.com>
2025-03-19 23:49:09 +0530

    Sync

---
+++ sync/async.js
@@ -0,0 +1,19 @@
+
+function oneTofive() {
+
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+
+}
+setTimeout(oneTofive,3000);
+
+// oneTofive();
+
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);
